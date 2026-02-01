0bea7492e38ab95397b37e34a42ffb3afa2907d6
Jared Pinfold <j.pinfold@gmail.com>
2026-02-01 14:42:53 +1300

    WIP on hangman game

--- client/components/games/Hangman.tsx
+++ client/components/games/Hangman.tsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from 'react'
 import { useCard } from '../../hooks/useCard'
 import { Error } from '../Error'
 import { Loading } from '../Loading'
 
 export function Hangman() {
+  // Fetch data for a random MTG card
   const { data: card, isError, isLoading } = useCard()
-  console.log(card)
+  
+  // State
+  const [guessed, setGuessed] = useState([] as string[])   // Used to determine if a letter on the keyboard is greyed out
+  const [remainingGuesses, setRemainingGuesses] = useState(5)
+  const [correctlyGuessed, setCorrectlyGuessed ] = useState([] as string[]) // Used to determine if a letter should appear on the answer gameboard
+
+  // Ensure punctuation and spaces are displayed in the answer gameboard
+  useEffect(()=> {
+    if (card) {
+      const punctuation = card.name.split('').filter(c => /[^a-zA-Z0-9]/.test(c))
+      setCorrectlyGuessed(s => [...s, ...punctuation])
+    }
+  }, [card])
+
+console.log('corrGuess', correctlyGuessed)
+  // Determine which letters are correct
+  const correctLetters = new Array(...new Set(card?.name.toLowerCase().split('')))
+console.log("corr", correctLetters)
+  // Which characters to display in the answer gameboard
+  const charactersOrUnderscores = card?.name
+    .split('')
+    .map((c) => {
+      if (correctlyGuessed.includes(c.toLowerCase())) return c
+      else return "_"
+    })
+    .join('')
+
   if (isError) return <Error />
   if (isLoading) return <Loading />
 
@@ -13,6 +41,7 @@ export function Hangman() {
       <>
         <h2>Hangman</h2>
         <p>{card.name}</p>
+        <p>{charactersOrUnderscores}</p>
         <img src={card.image} alt={''} />
       </>
     )
