b0a37bb1230715929005699ecded6d5d92a601c8
Jared Pinfold <j.pinfold@gmail.com>
2026-02-05 15:28:58 +1300

    update API to deployed version

--- client/apis/cards.ts
+++ client/apis/cards.ts
@@ -1,10 +1,15 @@
 import request from 'superagent'
 import { IHangmanCard } from '../../models/cards'
 
-const rootUrl = '/api/v1/cards'
+const rootUrl = {
+  local: '/api/v1/cards',
+  deployed: 'https://mtg-mini-games.netlify.app/api/v1/cards',
+}
+
+const environment = 'deployed'
 
 export function getRandomCardForHangman(): Promise<IHangmanCard> {
-  return request.get(rootUrl + '/hangman').then((res) => {
+  return request.get(rootUrl[environment] + '/hangman').then((res) => {
     return res.body
   })
 }
