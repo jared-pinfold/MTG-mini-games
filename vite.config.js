bcd5d88e07feed0297ac4574910fb5f6af3e1acb
Jared Pinfold <j.pinfold@gmail.com>
2026-03-02 16:51:37 +1300

    update config to make react router work on netlify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import netlifyReactRouter from "@netlify/vite-plugin-react-router"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlifyReactRouter()],
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
